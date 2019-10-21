66695ed4ab3b04061e25ac17787a03a57026b5c6
zhaichunlin <zhaichunlin@cmcm.com>
2019-10-21 11:10:43 +0800

    optimize

--- KKBTest/Lesson/Lesson-02/main.py
+++ KKBTest/Lesson/Lesson-02/main.py
@@ -24,21 +24,6 @@ def imgShow(img, type=0):
         plt.show()
 
 
-def customMedianBlur(scr, ksize=3):
-    if ksize % 2 == 0:
-        raise AssertionError("ksize % 2 == 1")
-    h, w = scr.shape[:2]
-    space = int((ksize-1)/2)
-    new_img = scr.copy()
-    for i in range(space, h - space):
-        for j in range(space, w - space):
-            new_img[i, j, 0] = np.median(scr[i - space:i + space + 1, j - space:j + space + 1, 0])  # B
-            new_img[i, j, 1] = np.median(scr[i - space:i + space + 1, j - space:j + space + 1, 1])  # G
-            new_img[i, j, 2] = np.median(scr[i - space:i + space + 1, j - space:j + space + 1, 2])  # R
-
-    return new_img
-
-
 def bgr2RGB(img):
     # BGR -> RGB
     # B, G, R = cv2.split(img)
@@ -46,6 +31,42 @@ def bgr2RGB(img):
 
     img_new = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return img_new
+
+
+def customMedianBlur(scr, ksize=3):
+    if ksize % 2 <= 0:
+        raise AssertionError("ksize % 2 == 1")
+
+    h, w = scr.shape[:2]
+    space = int((ksize - 1) / 2)
+    new_img = scr.copy()
+
+    for i in range(0, h):
+        for j in range(0, w):
+            if len(scr.shape) == 3:
+                new_img[i, j, 0] = np.median(arrForMedian(scr, i, j, space, 0))  # B
+                new_img[i, j, 1] = np.median(arrForMedian(scr, i, j, space, 1))  # G
+                new_img[i, j, 2] = np.median(arrForMedian(scr, i, j, space, 2))  # R
+            elif len(scr.shape) == 2:
+                new_img[i, j] = np.median(arrForMedian(scr, i, j, space))  # gray
+
+    return new_img
+
+
+def arrForMedian(scr, i, j, space, channel_id=-1):
+
+    h, w = scr.shape[:2]
+    if channel_id == -1:
+        return scr[i - space if (i - space) >= 0 else 0:
+               i + space + 1 if (i + space + 1) <= h else h,
+           j - space if (j - space) >= 0 else 0:
+           j + space + 1 if (j + space + 1) <= w else w]
+    else:
+        return scr[i - space if (i - space) >= 0 else 0:
+               i + space + 1 if (i + space + 1) <= h else h,
+           j - space if (j - space) >= 0 else 0:
+           j + space + 1 if (j + space + 1) <= w else w, channel_id]
+
 
 
 def addNoise(img):
@@ -63,7 +84,9 @@ def addNoise(img):
 
 if __name__ == '__main__':
     img = imreadModes()
-    noise_img = addNoise(img)
+    noise_img = addNoise(img)   # 生成噪点图
+    # noise_img = cv2.cvtColor(noise_img, cv2.COLOR_BGR2GRAY)     # 测试灰度图
     med_img = customMedianBlur(noise_img, 3)
-    cv2.imshow('test1', noise_img)
+    cv2.imshow('order_test', noise_img)
+    cv2.imshow('m_test', cv2.medianBlur(noise_img, 3))
     imgShow(med_img)
